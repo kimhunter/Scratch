9ea7c0e3d27ec5b3dd6b1b4ab2a8574c29271c89
Kim Hunter <bigkm1@gmail.com>
2013-07-31 22:43:32 +1000

    Add quick dirty profiling macro

--- Scratch/scratch.h
+++ Scratch/scratch.h
@@ -9,12 +9,22 @@
 #ifndef Scratch_scratch_h
 #define Scratch_scratch_h
 
+#include <time.h>
 
 #define LOGV(V) NSLog(@"%s = %@", #V, (V))
 #define LOGD(D) NSLog(@"%s = %ld", #D, (long)(D))
 #define LOGF(F) NSLog(@"%s = %lf", #F, (F))
 #define LOGR(R) NSLog(@"%s = %@", #R, NSStringFromRect(R))
 #define HERE()  NSLog(@"%s (Line:%d)", __PRETTY_FUNCTION__, __LINE__)
+
+
+#define KMProfile(MSG, CODE_TO_PROFILE) {           \
+    time_t km_start, km_end; double km_dur;         \
+    time(&km_start);                                \
+    CODE_TO_PROFILE                                 \
+    time(&km_end);                                  \
+    km_dur = difftime(km_end, km_start);            \
+    NSLog(@"KMProfile: %@ TOOK %.0lfms (%.1lfs) ", MSG, km_dur*1000, km_dur); }
 
 
 #endif
