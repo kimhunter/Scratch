eebcd291462f08d57d046ce8b6fdba684be76d2b
Kim Hunter <bigkm1@gmail.com>
2013-08-21 11:37:04 +0100

    Add c++ class

---
+++ Scratch/Red.h
@@ -0,0 +1,13 @@
+//
+//  Red.h
+//  Scratch
+//
+//  Created by Kim Hunter on 21/08/13.
+//  Copyright (c) 2013 Kim Hunter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Red : NSObject
+
+@end
---
+++ Scratch/Red.mm
@@ -0,0 +1,13 @@
+//
+//  Red.m
+//  Scratch
+//
+//  Created by Kim Hunter on 21/08/13.
+//  Copyright (c) 2013 Kim Hunter. All rights reserved.
+//
+
+#import "Red.h"
+
+@implementation Red
+
+@end
