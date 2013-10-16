9b1b8e2abbd4a6e6f4d20a5ec569847c82a7c4e6
Kim Hunter <bigkm1@gmail.com>
2013-10-16 06:35:44 +0100

    Add Private Header

---
+++ Scratch/Purple.h
@@ -0,0 +1,13 @@
+//
+//  Purple.h
+//  Scratch
+//
+//  Created by Kim Hunter on 29/09/13.
+//  Copyright (c) 2013 Kim Hunter. All rights reserved.
+//
+
+#import "Blue.h"
+
+@interface Purple : Blue
+
+@end
---
+++ Scratch/Purple.m
@@ -0,0 +1,13 @@
+//
+//  Purple.m
+//  Scratch
+//
+//  Created by Kim Hunter on 29/09/13.
+//  Copyright (c) 2013 Kim Hunter. All rights reserved.
+//
+
+#import "Purple.h"
+
+@implementation Purple
+
+@end
